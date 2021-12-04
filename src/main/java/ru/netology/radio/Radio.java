497c96dd9bd04bf674f1bd6589685335e67b85de
Andrey Martyn <martynandrey@rambler.ru>
2021-12-04 18:02:04 +0600

    Realized Radio class with control methods

--- src/main/java/ru/netology/radio/Radio.java
+++ src/main/java/ru/netology/radio/Radio.java
@@ -1,4 +1,54 @@
 package ru.netology.radio;
 
 public class Radio {
+    private int currentStation;
+    private int currentVolume;
+
+    public int getCurrentStation() {
+        return currentStation;
+    }
+
+    public void setCurrentStation(int station) {
+        if (station < 0) {
+            return;
+        }
+        if (station > 9) {
+            return;
+        }
+        currentStation = station;
+    }
+
+    public void nextStation() {
+        if (currentStation == 9) {
+            currentStation = 0;
+        }
+        else {
+            currentStation++;
+        }
+    }
+
+    public void prevStation() {
+        if (currentStation == 0) {
+            currentStation = 9;
+        }
+        else {
+            currentStation--;
+        }
+    }
+
+    public int getCurrentVolume() {
+        return currentVolume;
+    }
+
+    public void increaseVolume() {
+        if (currentVolume < 10) {
+            currentVolume++;
+        }
+    }
+
+    public void reduceVolume() {
+        if (currentVolume > 0) {
+            currentVolume--;
+        }
+    }
 }
